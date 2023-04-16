f20a92d8830133c69bb516a1701eb8834067a481
Olivier PERDRIX <olivierperdrix@live.fr>
2023-04-16 17:06:39 +0200

    fin chap 5-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const Title = ({ content }) => <h1 id="title">{content}</h1>
+const Component = ({ title, onClick }) => <button onClick={() => onClick(title)}>Learn {title}</button>
 
 function App() {
+  const [title, setTitle] = useState("React");
+  const handleOnClick = (lib) => setTitle(lib)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Title content={title} />
+        <Component onClick={handleOnClick} title="React" />
+        <Component onClick={handleOnClick} title="Angular" />
+        <Component onClick={handleOnClick} title="Vue" />
+    </header>  
     </div>
   );
 }
