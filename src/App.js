4a4ddaf6200b0040d4462f5f7ce56fda80847a9b
Olivier PERDRIX <olivierperdrix@live.fr>
2023-04-16 16:13:41 +0200

    fin chapitre 4-6

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
+import logoPink from './logo-pink.svg';
 import './App.css';
 
+const Logo = () => <img id="logo" src={logo} className="App-logo" alt="logo" />
+
 function App() {
+  const handleOnClick = (svg) => {
+    const logo = document.getElementById('logo');
+    logo.src = svg;
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+        <button onClick={() => handleOnClick(logo)}>blue</button>
+        <button onClick={() => handleOnClick(logoPink)}>pink</button>
       </header>
     </div>
   );
