18a990718d2c5231cf5874b4e90e3f6d55f07806
Olivier PERDRIX <olivierperdrix@live.fr>
2023-04-16 16:20:59 +0200

    début chap 4-8

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+const Title = ({ content }) => <h1>{content}</h1>;
+const ComponentReact = () => {
+  return (
+    <>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <br />
+    </>
+  );
+};
+const ComponentAngular = () => {
+  return (
+    <>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn Angular
+      </a>
+      <br />
+    </>
+  );
+};
+const ComponentVue = () => {
+  return (
+    <>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn Vue
+      </a>
+      <br />
+    </>
+  );
+};
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title content="Welcome !" />
+        <ComponentReact />
+        <ComponentVue />
+        <ComponentAngular />
       </header>
     </div>
   );
