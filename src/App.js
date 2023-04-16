a0680daeff71f2390a290ab274d3c13bb7db5b08
Olivier PERDRIX <olivierperdrix@live.fr>
2023-04-16 19:24:59 +0200

    fin chap 6-6

--- src/App.js
+++ src/App.js
@@ -2,56 +2,61 @@ import { useState } from "react";
 import './App.css';
 
 function Header() {
-  return(<h1>Les librairies Front</h1>)
+  return (<h1>Les librairies Front</h1>)
 }
-function Component({ title, link, isSelected}) {
-  return(
+function Component({ title, link, isSelected }) {
+  return (
     <li className={isSelected ? "component blue" : "component"}>
       <a href={link} target="_blank" rel="nonopener noreferrer">{title}</a>
     </li>
   )
 }
 
-function Form ({ onChange, onSubmit }) {
-  return(
-  <form onSubmit={onSubmit} className="flex space-between">
-    <input type="text" onChange={onChange}  placeholder="title" name="title" />
-    <input type="text" onChange={onChange}  placeholder="link"  name="link" />
-    <button type="submit">add</button>
-  </form>)
+function Form({ onChange, onSubmit }) {
+  return (
+    <form onSubmit={onSubmit} className="flex space-between">
+      <input type="text" onChange={onChange} placeholder="title" name="title" />
+      <input type="text" onChange={onChange} placeholder="link" name="link" />
+      <button type="submit">add</button>
+    </form>)
 }
 
 function App() {
   const libraries = [
-    {title: "React", link: "https://reactjs.org/"}, 
-    {title: "Angular", link: "https://angular.io/"}, 
-    {title:"Vue", link: "https://vuejs.org/"}, 
-    {title:"Bootstrap", link: "https://getbootstrap.com/"}
+    { title: "React", link: "https://reactjs.org/" },
+    { title: "Angular", link: "https://angular.io/" },
+    { title: "Vue", link: "https://vuejs.org/" },
+    { title: "Bootstrap", link: "https://getbootstrap.com/" }
   ];
 
   const [input, setInput] = useState(null);
   const [items, setItems] = useState(libraries)
-  const handleOnChange = e => setInput({... input, [e.target.name]: e.target.value})
+  const handleOnChange = e => setInput({ ...input, [e.target.name]: e.target.value })
   const handleOnSubmit = e => {
     e.preventDefault()
-    // setItems
+    if (!input?.title || !input?.link) {
+      console.log("error")
+      return
+    }
+    setItems([input, ...items])
+    setInput(null)
   }
   return (
     <div className="App">
-        <Header />
-        <Form onChange={handleOnChange} onSubmit={handleOnSubmit}/>
-        <ul className="list">
-          {items.map((lib, i) => {
-            return(
-                    <Component 
-                      key={lib.link}
-                      title={lib.title}
-                      index={i}
-                      link={lib.link}
-                    />
-                )
-            })}
-        </ul>
+      <Header />
+      <Form onChange={handleOnChange} onSubmit={handleOnSubmit} />
+      <ul className="list">
+        {items.map((lib, i) => {
+          return (
+            <Component
+              key={lib.link}
+              title={lib.title}
+              index={i}
+              link={lib.link}
+            />
+          )
+        })}
+      </ul>
     </div>
   );
 }
