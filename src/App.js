ff9827acfef0fb94155d3b2174bc7b2543d410c6
Olivier PERDRIX <olivierperdrix@live.fr>
2023-04-16 18:42:23 +0200

    fin chap6-2 6-3

--- src/App.js
+++ src/App.js
@@ -2,33 +2,46 @@ import { useState } from "react";
 import "./App.css";
 
 function Header({ title }) {
-  return(<h1>{ title }</h1>)
+  return (<h1>{title}</h1>)
 }
-function Component({ title, onClick }) {
-  
-  return(
-    <div className="component">
-      <p>{title}</p>
-      <button onClick={() => onClick(title)}>click me !</button>
+function Component({ title, index, link, onClick, isSelected }) {
+
+  return (
+    <div className={isSelected ? "component green": "component"}>
+      <p><a href={link} target="_blank" rel="nonopener noreferrer">{title}</a></p>
+      <button onClick={() => onClick(title, index)}>click me !</button>
     </div>)
 }
 
 function App() {
   const libraries = [
-    {title: "React", link: "https://reactjs.org/"}, 
-    {title: "Angular", link: "https://angular.io/"}, 
-    {title:"Vue", link: "https://vuejs.org/"}, 
+    { title: "React", link: "https://reactjs.org/" },
+    { title: "Angular", link: "https://angular.io/" },
+    { title: "Vue", link: "https://vuejs.org/" },
+    { title: "Bootstrap", link: "https://getbbotstrap.com/" },
   ];
+  const [index, setIndex] = useState(0)
   const [title, setTitle] = useState("React")
-  const handleOnClick = (value) => setTitle(value) 
+  const handleOnClick = (value, index) => {
+    setTitle(value)
+    setIndex(index)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <Header title={title} />
-        <Component title="Learn React"  link="https://reactjs.org/" onClick={handleOnClick} />
-        <Component title="Learn Angular" link="https://angular.io/" onClick={handleOnClick}/>
-        <Component title="Learn Vue" link="https://vuejs.org/" onClick={handleOnClick}/>
-    </header>  
+        {libraries.map((lib, i) => {
+          return (
+            <Component
+              isSelected={index === i}
+              key={lib.link}
+              index={i}
+              title={lib.title}
+              link={lib.link}
+              onClick={handleOnClick} />
+              )
+        })}
+      </header>
     </div>
   );
 }
